ab0f737e357ee9759242aff87e9092dc120f4eff
Derek Williams <jogamer130@gmail.com>
2020-03-19 15:36:19 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,7 +6,7 @@ require 'directors_database'
 def gross_for_director(director_data)
   movie_index = 0 
   gross_total = 0 
-  while movie_index < director_data[:movie].length do
+  while movie_index < director_data[:movies].length do
      gross_total += director_data[:movies][movie_index][:worldwide_gross]
       movie_index += 1 
     end
